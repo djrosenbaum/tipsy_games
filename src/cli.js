0e97cf3dfbc54eb2cb50a2ae34c585b54c4f4f68
DJ Rosenbaum <djrosenbaum@gmail.com>
2020-12-05 20:16:19 -0500

    refactor cli

--- src/cli.js
+++ src/cli.js
@@ -5,35 +5,42 @@ import fs from 'fs';
 import path from 'path';
 import { execSync } from 'child_process';
 
-// check the node version
-const nvmrc = fs.readFileSync('.nvmrc', 'utf8');
-if (nvmrc !== process.version) {
-  console.log(
-    `Expected Node Version ${nvmrc} but instead found ${process.version} \n`
-  );
-  process.exit();
-}
+const checkNodeVersion = () => {
+  // check the node version
+  const nvmrc = fs.readFileSync('.nvmrc', 'utf8');
+  if (nvmrc !== process.version) {
+    console.log(
+      `Expected Node Version ${nvmrc} but instead found ${process.version} \n Try typing "nvm use" to switch node versions \n`
+    );
+    process.exit();
+  }
+};
 
-function getGameList() {
+const getGameList = () => {
   const projects = path.join('', 'projects');
   const files = fs.readdirSync(projects);
 
   return files.filter((file) => !file.startsWith('.'));
-}
+};
 
-function buildGame(answer) {
+const buildGame = (answer) => {
   execSync(`CLIENT=${answer.game} npm run dev`, {
     stdio: 'inherit',
   });
-}
+};
 
-inquirer
-  .prompt([
-    {
-      type: 'list',
-      name: 'game',
-      message: 'select a game',
-      choices: getGameList(),
-    },
-  ])
-  .then(buildGame);
+const inquire = () => {
+  inquirer
+    .prompt([
+      {
+        type: 'list',
+        name: 'game',
+        message: 'select a game',
+        choices: getGameList(),
+      },
+    ])
+    .then(buildGame);
+};
+
+checkNodeVersion();
+inquire();
